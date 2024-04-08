9697400ed2d8c01d84927dcb77b14cf35046adfa
Patrick Schroen <ps@ufotechnologies.com>
2024-04-08 10:10:30 -0400

    Add Slider desktop functionality

--- src/js/components/Slider.js
+++ src/js/components/Slider.js
@@ -15,26 +15,108 @@ export class Slider extends Component {
         this.render();
 
         this.el = this.nodeList[0];
-        this.container = this.el.querySelector('.slider-container');
+        this.slider = this.el.querySelector('.slider-container');
 
         this.sectionData.images.forEach(data => {
             const figure = new Figure(data);
-            this.container.append(figure.el);
+            this.slider.append(figure.el);
         });
+
+        if (!navigator.maxTouchPoints) {
+            this.isDown = false;
+            this.startX = 0;
+            this.scrollLeft = 0;
+            this.minDistance = 8;
+
+            this.enable();
+        }
     }
 
     render() {
         this.nodeList = html(/* html */ `
             <section class="slider lazy">
                 <div class="slider-wrapper">
-                    <div class="slider-container">
+                    <div class="slider-container grabbable">
                     </div>
                 </div>
             </section>
         `);
     }
 
+    addListeners() {
+        this.slider.addEventListener('mousedown', this.onMouseDown);
+        this.slider.addEventListener('mousemove', this.onMouseMove);
+        this.slider.addEventListener('mouseleave', this.onMouseLeave);
+        this.slider.addEventListener('click', this.onClick);
+    }
+
+    removeListeners() {
+        this.slider.removeEventListener('mousedown', this.onMouseDown);
+        this.slider.removeEventListener('mousemove', this.onMouseMove);
+        this.slider.removeEventListener('mouseleave', this.onMouseLeave);
+        this.slider.removeEventListener('click', this.onClick);
+    }
+
+    // Event handlers
+
+    onMouseDown = e => {
+        this.isDown = true;
+
+        this.slider.classList.add('active');
+
+        this.startX = e.pageX - this.slider.offsetLeft;
+        this.scrollLeft = this.slider.scrollLeft;
+    };
+
+    onMouseMove = e => {
+        if (!this.isDown) {
+            return;
+        }
+
+        e.preventDefault();
+
+        const x = e.pageX - this.slider.offsetLeft;
+        const walk = x - this.startX;
+
+        this.slider.scrollLeft = this.scrollLeft - walk;
+    };
+
+    onMouseLeave = () => {
+        this.isDown = false;
+
+        this.slider.classList.remove('active');
+    };
+
+    onClick = e => {
+        this.isDown = false;
+
+        this.slider.classList.remove('active');
+
+        const x = e.pageX - this.slider.offsetLeft;
+        const walk = x - this.startX;
+
+        if (Math.abs(walk) > this.minDistance) {
+            e.preventDefault();
+        }
+    };
+
+    // Public methods
+
     animateIn = () => {
         this.el.classList.add('is-loaded');
+    };
+
+    enable = () => {
+        this.addListeners();
+    };
+
+    disable = () => {
+        this.removeListeners();
+    };
+
+    destroy = () => {
+        this.disable();
+
+        return super.destroy();
     };
 }
